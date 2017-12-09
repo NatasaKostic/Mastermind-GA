57618e359e81e659aec281d9d4a5d5bb9947bc83
NatasaKostic <nmk44@cornell.edu>
2017-12-09 15:14:45 -0500

    fixing fitness fxn

--- AI_code_breaker.py
+++ AI_code_breaker.py
@@ -6,45 +6,120 @@ import tkinter
 import random
 
 P_CROSS_OVER = 0.5
-P_INVERT = 0.02
+P_INVERT = 0.03
 P_MUTATE = 0.03
+P_PERMUTE = 0.03
 MAX_GEN = 100
-MAX_POP = 10
+MAX_POP = 60
+REQ_FIT = 0.6
+
+
+def remove_dups(seq):
+   # Not order preserving
+   keys = {}
+   for e in seq:
+       keys[e] = 1
+   return keys.keys()
 
 
 def GA(n_peg, n_col, guess, response):
 
 
-# fotness fxn that's inversely proportional to the difference between the solution and the value a decoded chromosome represents
+# fitness fxn that's inversely proportional to the difference between the solution and the value a decoded chromosome represents
 
 
 	def cross_over(g1,g2):
-		if random.random() >= P_CROSS_OVER:
-			cross_loc = random.randint(1, n_peg-1)
-			new_g = g1[0:cross_loc] + g2[cross_loc:]
+		#if random.random() < P_CROSS_OVER:
+		cross_loc = random.randint(1, n_peg-1)
+		new_g1 = g1[0:cross_loc] + g2[cross_loc:]
+		new_g2 = g2[0:cross_loc] + g1[cross_loc:]
+		return([new_g1, new_g2])
 
 
-	
-	h = 1
-	# E = set of eligible codes at iteration i
-	E_i = []
+	def mutate(g1, n_peg, n_col):
+		if(random.random() < P_MUTATE):
+			loc = random.randint(0, n_peg-1)
+			col = random.randint(1, n_col)
+			g1[loc] = col
+		return(g1)
 
-	# initialize the population
+	def get_fitness(g1, g2, response):
+		X_i = response[1]
+		Y_i = response[2]
+		Xi_g2 = 0
+		for i in range(len(g1)):
+			if(g1[i] == g2[i]):
+				Xi_g2 += 1
+			
+		
+		fitness_g2 = X_i + 1/2 * Y_i
+		return(fitness_g2)
+
+
+
+
+	# initialize the population and remove any duplicates
 	population = [[random.randint(1, n_col) for i in range(n_peg)] for j in range(MAX_POP)]
 	
+	# h = iteration number
+	h = 1
+	# E = set of eligible/elite codes at iteration h
+	E_h = []
 	
-	while(h <= MAX_GEN and len(E_i) <= MAX_POP):
-		# generate new pop with crossover, mutation, inversion, permutation
-		#child = crossover()
-		# calc fitness
-
-		# add eligible combos to E if not there yet
-		h += 1
-
-	#play something from E 
+	while(h <= MAX_GEN and len(E_h) <= MAX_POP):
 
 
+		# first evaluate the population and take some of the best + some random
+		fitness = []
+		for indiv in population:
+			fitness.append(get_fitness(guess, indiv, response))
 
+		# the new generation will contain some of the best indivs in this gen + randoms,
+		# crossed over offspring of some,
+		# mutations of some
+		# use fitness values as probabilities to choose best indivs to keep same
+		new_gen = numpy.random.choice(population, size = MAX_POP*(1-P_CROSS_OVER), replace=False, p = fitness)
+
+		# select parents from previous gen using fitness values as probabilities
+		parents = numpy.random.choice(population, size= MAX_POP*P_CROSS_OVER-10, replace=False, p = fitness)
+		# add some random indiv to parental pool promote genetic diversity
+		while len(parents) < MAX_POP*P_CROSS_OVER:
+			parents.append([random.randint(1, n_col) for i in range(n_peg)])
+
+		random.shuffle(parents)
+
+		children = []
+		i = 0
+		while i < len(parents):
+			child = cross_over(parents[i], parents[i+1])
+			if random.random() <= CROSSOVER_THEN_MUTATION_PROBABILITY:
+				child = [mutate(child[1]), mutate(child[2])]
+			children.extend(child)
+			i += 2
+
+		# add the kids to the new population
+		new_gen.extend(children)
+
+		#get fitness of the elements in this new gen
+		fitness = []
+		for indiv in new_gen:
+			fitness.append(get_fitness(guess,indiv, response))
+
+		# add eligible codes to E_h e.g. codes with fitness above a required value
+		for f in range(len(fitness)):
+			if fitness[f] >= REQ_FIT:
+				E_h.append(new_gen[f])
+
+		# remove dups in eligibles
+		E_h = remove_dups(E_h)
+
+		population = E_h
+		# fill remaining spot with random and remove duplicates again
+		while len(population) < MAX_POP:
+			population.append([random.randint(1, n_col) for i in range(n_peg)])
+			population = remove_dups(population)
+
+	return E_h
 
 
 def AI_play(code, n_col, n_peg, response):
@@ -90,7 +165,7 @@ def person_play(guess, code):
 
 
 # method that controls the alternating turns
-def play(n_col, n_peg, n_turns,code):
+def play(n_col, n_peg, n_turns):
 	
 	turn = 1
 	response = NULL
@@ -120,10 +195,6 @@ def main():
 	num_colors = int(sys.argv[1])
 	# the number of pegs that can be used in a code
 	num_pegs = int(sys.argv[2])
-
-
-
-
 
 
 
